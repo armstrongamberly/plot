5cc0fae121b857140e8f550ae1a03f72256bde08
Amberly <armstrong.amberly@gmail.com>
2017-10-23 17:35:40 -0400

    added login page and added route in userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,32 @@ class UsersController < ApplicationController
     # end
     erb :"/users/create_users"
   end
+
+  # post "/signup" do
+  #   if params.has_value?("")
+  #     redirect "/signup"
+  #   end
+  #   @user = User.create(:username => params[:username], email: params[:email], :password => params[:password])
+  #   session[:user_id] = @user.id
+  #   redirect '/markers/index'
+  # end
+
+  get '/login' do
+    # if logged_in?
+    #   redirect "/tweets"
+    # end
+    erb :"/users/login"
+  end
+
+#   post "/login" do
+#     @user = User.find_by(:username => params[:username])
+#     if @user && @user.authenticate(params[:password])
+#       session[:user_id] = @user.id
+#       redirect "/tweets"
+#     # else
+#     #   redirect "/login"
+#     # end
+# end
+  # end
+
 end
